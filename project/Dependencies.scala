e74cb6d612658fbaacc3dc345c8bc47c55f8ec8f
Scala Steward <me@scala-steward.org>
2022-02-17 14:11:53 +0100

    Update otel-tools_2.13 to 0.1.10

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -23,7 +23,7 @@ object Dependencies {
     val LogstashLogbackVersion: String = "6.3"
     val OpenTelemetryVersion: String   = "1.6.0"
     val TestContainers: String         = "0.40.1"
-    val OtelToolsVersion: String       = "0.1.9"
+    val OtelToolsVersion: String       = "0.1.10"
   }
 
   import Dependencies.Versions._
