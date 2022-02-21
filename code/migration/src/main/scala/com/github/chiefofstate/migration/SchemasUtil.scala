94a76c550f4fa0751981ee7001c6f9b82115924d
Arsene <tochemey@hotmail.com>
2022-02-21 02:25:12 +0000

    chore: remove legacy code (#15)

--- code/migration/src/main/scala/com/github/chiefofstate/migration/SchemasUtil.scala
+++ code/migration/src/main/scala/com/github/chiefofstate/migration/SchemasUtil.scala
@@ -159,20 +159,6 @@ object SchemasUtil {
   }
 
   /**
-   * drops the legacy journal and snapshot tables
-   *
-   * @param journalJdbcConfig the database config
-   */
-  val dropLegacyJournalTablesStmt: SqlAction[Int, NoStream, Effect] = {
-    sqlu"""
-             DROP TABLE IF EXISTS
-                journal,
-                snapshot
-             CASCADE
-            """
-  }
-
-  /**
    * drops the journal and snapshot tables
    */
   def dropJournalTables(journalJdbcConfig: DatabaseConfig[JdbcProfile]): Unit = {
@@ -192,39 +178,5 @@ object SchemasUtil {
       .transactionally
 
     Await.result(journalJdbcConfig.db.run(stmt), Duration.Inf)
-  }
-
-  /**
-   * helper method merely for tests
-   *
-   * @param journalJdbcConfig the jdbc config
-   */
-  private[migration] def createLegacyJournalAndSnapshot(journalJdbcConfig: DatabaseConfig[JdbcProfile]): Unit = {
-    val createStmt: SqlAction[Int, NoStream, Effect] = sqlu"""
-     CREATE TABLE IF NOT EXISTS journal (
-      ordering        BIGSERIAL,
-      persistence_id  VARCHAR(255) NOT NULL,
-      sequence_number BIGINT       NOT NULL,
-      deleted         BOOLEAN      DEFAULT FALSE,
-      tags            VARCHAR(255) DEFAULT NULL,
-      message         BYTEA        NOT NULL,
-      PRIMARY KEY (persistence_id, sequence_number)
-     )"""
-
-    val ixStmt: SqlAction[Int, NoStream, Effect] =
-      sqlu"""CREATE UNIQUE INDEX IF NOT EXISTS journal_ordering_idx on journal(ordering)"""
-
-    val snpashotStmt: SqlAction[Int, NoStream, Effect] = sqlu"""
-     CREATE TABLE IF NOT EXISTS snapshot (
-      persistence_id  VARCHAR(255) NOT NULL,
-      sequence_number BIGINT       NOT NULL,
-      created         BIGINT       NOT NULL,
-      snapshot        BYTEA        NOT NULL,
-      PRIMARY KEY (persistence_id, sequence_number)
-     )"""
-
-    val actions: DBIO[Unit] = DBIO.seq(createStmt, ixStmt, snpashotStmt).withPinnedSession.transactionally
-
-    Await.result(journalJdbcConfig.db.run(actions), Duration.Inf)
   }
 }
