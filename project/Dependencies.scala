a3177bbd961081d5f1e8f0eb26df928309bca455
Scala Steward <scala_steward@virtuslab.com>
2022-11-12 14:38:46 +0000

    Update jackson-module-scala to 2.14.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -29,7 +29,7 @@ object Dependencies {
     val OpenTelemetryExtensionVersion: String = "1.19.0"
     val OpenTelemetryGRPCVersion: String = "1.0.1-alpha"
     val ScalaXmlVersion: String = "2.1.0"
-    val JacksonVersion: String = "2.13.4"
+    val JacksonVersion: String = "2.14.0"
   }
 
   import Dependencies.Versions._
