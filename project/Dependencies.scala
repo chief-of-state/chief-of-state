74ec5cce007ca6bc9667ab7cd9f90dc83dcbd97f
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2022-11-22 20:23:58 +0100

    build: update jackson-annotations, jackson-core, ... to 2.14.1 (#134)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -29,7 +29,7 @@ object Dependencies {
     val OpenTelemetryExtensionVersion: String = "1.20.1"
     val OpenTelemetryGRPCVersion: String = "1.0.1-alpha"
     val ScalaXmlVersion: String = "2.1.0"
-    val JacksonVersion: String = "2.14.0"
+    val JacksonVersion: String = "2.14.1"
   }
 
   import Dependencies.Versions._
