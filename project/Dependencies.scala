7e54ba567a5ad94b3a60e61c42c2a58a49a9ba33
Scala Steward <scala_steward@virtuslab.com>
2023-03-18 14:31:00 +0000

    Update postgresql to 42.6.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -14,7 +14,7 @@ object Dependencies {
     val ScalaTestVersion: String = "3.2.15"
     val AkkaManagementVersion: String = "1.1.4"
     val AkkaProjectionVersion: String = "1.2.5"
-    val PostgresDriverVersion: String = "42.5.4"
+    val PostgresDriverVersion: String = "42.6.0"
     val SlickVersion: String = "3.3.3"
     val AkkaPersistenceJdbcVersion: String = "5.1.0"
     val ScalaMockVersion: String = "5.2.0"
