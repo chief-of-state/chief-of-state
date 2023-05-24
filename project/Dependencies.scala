f8b4cbbf1af000802a50b7930b144c682f0b8922
Scala Steward <scala_steward@virtuslab.com>
2023-05-24 16:10:26 +0000

    Update scalatest to 3.2.16

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -11,7 +11,7 @@ object Dependencies {
     val SilencerVersion: String = "1.7.12"
     val LogbackVersion: String = "1.4.7"
     val ScalapbValidationVersion: String = "0.1.4"
-    val ScalaTestVersion: String = "3.2.15"
+    val ScalaTestVersion: String = "3.2.16"
     val AkkaManagementVersion: String = "1.1.4"
     val AkkaProjectionVersion: String = "1.2.5"
     val PostgresDriverVersion: String = "42.6.0"
