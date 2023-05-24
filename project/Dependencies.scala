85b2bdb1d2831b2d977faf1178c2950d2c646ee9
Scala Steward <scala_steward@virtuslab.com>
2023-05-24 16:09:14 +0000

    Update jackson-dataformat-yaml to 2.15.1

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -29,7 +29,7 @@ object Dependencies {
     val OpenTelemetryExtensionVersion: String = "1.25.0"
     val OpenTelemetryGRPCVersion: String = "1.0.1-alpha"
     val ScalaXmlVersion: String = "2.1.0"
-    val JacksonVersion: String = "2.15.0"
+    val JacksonVersion: String = "2.15.1"
   }
 
   import Dependencies.Versions._
