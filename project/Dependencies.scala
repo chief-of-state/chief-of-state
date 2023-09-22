5c401894c4e35f3de76718132541f743ef80e9ed
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2023-09-22 22:00:54 +0200

    chore(deps): update scalatest to 3.2.17 (#248)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -11,7 +11,7 @@ object Dependencies {
     val SilencerVersion: String            = "1.17.13"
     val LogbackVersion: String             = "1.4.11"
     val ScalapbValidationVersion: String   = "0.1.4"
-    val ScalaTestVersion: String           = "3.2.16"
+    val ScalaTestVersion: String           = "3.2.17"
     val AkkaManagementVersion: String      = "1.1.4"
     val AkkaProjectionVersion: String      = "1.2.5"
     val PostgresDriverVersion: String      = "42.6.0"
