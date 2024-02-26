b5af67e11558e858940cd023e94554a3ead8e17c
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2024-02-26 01:02:09 +0100

    chore(deps): update postgresql to 42.7.2 (#297)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -14,7 +14,7 @@ object Dependencies {
     val ScalaTestVersion: String           = "3.2.18"
     val AkkaManagementVersion: String      = "1.1.4"
     val AkkaProjectionVersion: String      = "1.2.5"
-    val PostgresDriverVersion: String      = "42.7.1"
+    val PostgresDriverVersion: String      = "42.7.2"
     val SlickVersion: String               = "3.3.3"
     val AkkaPersistenceJdbcVersion: String = "5.1.0"
     val ScalaMockVersion: String           = "5.2.0"
