144bb688ca41490485e06a8651927c47f3b295d1
Scala Steward <scala_steward@virtuslab.com>
2024-03-10 13:44:43 +0000

    chore(deps): update jackson-annotations, jackson-core, ... to 2.16.2

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -29,7 +29,7 @@ object Dependencies {
     val OpenTelemetryExtensionVersion: String          = "1.35.0"
     val OpenTelemetryGRPCVersion: String               = "1.0.1-alpha"
     val ScalaXmlVersion: String                        = "2.2.0"
-    val JacksonVersion: String                         = "2.16.1"
+    val JacksonVersion: String                         = "2.16.2"
   }
 
   import Dependencies.Versions._
