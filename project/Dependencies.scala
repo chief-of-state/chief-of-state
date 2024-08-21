3284ec24a080971ed1ece46b14b000cfebbd5809
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2024-08-21 21:40:08 +0200

    chore(deps): update logback-classic to 1.5.7 (#354)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -11,7 +11,7 @@ object Dependencies {
     val AkkaVersion: String                = "2.6.20"
     val ScalapbCommonProtoVersion: String  = "2.9.6-0"
     val SilencerVersion: String            = "1.7.17"
-    val LogbackVersion: String             = "1.5.6"
+    val LogbackVersion: String             = "1.5.7"
     val ScalapbValidationVersion: String   = "0.1.4"
     val ScalaTestVersion: String           = "3.2.19"
     val AkkaManagementVersion: String      = "1.1.4"
