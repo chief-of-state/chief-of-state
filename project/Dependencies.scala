98d85b7d6069371b3875a6d90e2d831480cb88c5
Scala Steward <scala_steward@virtuslab.com>
2024-09-30 13:50:48 +0000

    chore(deps): update slick, slick-hikaricp to 3.5.2

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -16,7 +16,7 @@ object Dependencies {
     val PekkoManagementVersion: String      = "1.0.0"
     val PekkoProjectionVersion: String      = "1.0.0"
     val PostgresDriverVersion: String       = "42.7.4"
-    val SlickVersion: String                = "3.5.1"
+    val SlickVersion: String                = "3.5.2"
     val PekkoPersistenceJdbcVersion: String = "1.1.0"
     val ScalaMockVersion: String            = "6.0.0"
 
