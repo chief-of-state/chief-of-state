5eedbff226a00afb21665b0cfb207cab7799be36
Scala Steward <scala_steward@virtuslab.com>
2024-10-20 14:08:54 +0000

    chore(deps): update opentelemetry-instrumentation-annotations, ... to 2.9.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -25,7 +25,7 @@ object Dependencies {
     val OpenTelemetrySdkVersion: String                = "1.43.0"
     val TestContainers: String                         = "0.41.4"
     val OpenTelemetrySdkConfigVersion: String          = "1.43.0"
-    val OpenTelemetryInstrumentationApiVersion: String = "2.8.0"
+    val OpenTelemetryInstrumentationApiVersion: String = "2.9.0"
     val OpenTelemetrySdkTestingVersion: String         = "1.43.0"
     val OpenTelemetryExtensionVersion: String          = "1.43.0"
     val ScalaXmlVersion: String                        = "2.3.0"
