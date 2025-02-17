d302f0f8598a5f5a4dcdd43bb65bc77982a296c7
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2025-02-17 20:17:09 +0100

    chore(deps): update opentelemetry-instrumentation-annotations, ... to 2.13.0 (#456)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -25,7 +25,7 @@ object Dependencies {
     val OpenTelemetrySdkVersion: String                = "1.47.0"
     val TestContainers: String                         = "0.41.8"
     val OpenTelemetrySdkConfigVersion: String          = "1.47.0"
-    val OpenTelemetryInstrumentationApiVersion: String = "2.12.0"
+    val OpenTelemetryInstrumentationApiVersion: String = "2.13.0"
     val OpenTelemetrySdkTestingVersion: String         = "1.47.0"
     val OpenTelemetryExtensionVersion: String          = "1.47.0"
     val OpenTelemetryApiVersion: String                = "1.47.0"
