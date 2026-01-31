2a0b71235d7b11b653db8a5ba79964c83a395658
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2026-01-31 22:26:18 +0100

    chore(deps): update pekko-persistence-jdbc to 1.2.0 (#563)
    Signed-off-by: Arsene <tochemey@hotmail.com>
    Co-authored-by: Arsene <tochemey@hotmail.com>

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -18,7 +18,7 @@ object Dependencies {
     val PekkoProjectionVersion: String      = "1.1.0"
     val PostgresDriverVersion: String       = "42.7.9"
     val SlickVersion: String                = "3.6.1"
-    val PekkoPersistenceJdbcVersion: String = "1.1.1"
+    val PekkoPersistenceJdbcVersion: String = "1.2.0"
     val ScalaMockVersion: String            = "7.4.2"
 
     val JaninoVersion: String                          = "3.1.12"
